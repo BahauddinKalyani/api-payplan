80852cd50950115f3297a798e245f7e9024cae29
Bahauddin Kalyani <bahauddinkalyani@gmail.com>
2024-11-19 17:41:32 -0500

    fix add month logic

--- services/main_service.py
+++ services/main_service.py
@@ -5,15 +5,9 @@ from decimal import Decimal
 import calendar
 from datetime import date, timedelta, datetime
 from typing import List
-import boto3
 from botocore.exceptions import ClientError
-from fastapi import HTTPException
-# from models.transactions import Transaction, TransactionType, IncomeTransaction, ExpenseTransaction
 from uuid import uuid4
 from boto3.dynamodb.conditions import Key
-# from cognito_service import cognito_client
-from config.settings import settings
-from config.settings import dbconf
 from utils.transactions import get_transaction_service
 from models.transaction import Transaction
 from calendar import monthrange
@@ -25,11 +19,6 @@ logger = logging.getLogger(__name__)
 
 class MainService:
     def __init__(self, user_id: str):
-        # self.client = boto3.resource('dynamodb',
-        #                              region_name=settings.AWS_REGION,
-        #                              aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-        #                              aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
-        # self.table = self.client.Table(dbconf.Transaction_Table_Name)
         self.user_id = user_id
         self.today = datetime.now()
         self.start_range = self.today - timedelta(days=30)
@@ -38,11 +27,6 @@ class MainService:
         self.income_transactions = []
         self.expense_transactions = []
         self.current_balance = Decimal(0)
-        # today = datetime.now()
-        # start_window = today - timedelta(days=30)
-        # end_window = today + timedelta(days=210)
-        # self.recurring_income_transactions = {date: [] for date in self.date_range(start_window, end_window)}
-        # self.recurring_expense_transactions = {date: [] for date in self.date_range(start_window, end_window)}
 
     def parse_date(self, date_str: Optional[str]) -> Optional[datetime]:
         """Convert string date to datetime object."""
@@ -60,93 +44,6 @@ class MainService:
                 self.income_transactions.append(transaction)
             elif transaction.type == 'expense':
                 self.expense_transactions.append(transaction)
-                
-    # def update_balance_for_income(self, transaction: Dict):
-    #     """Update balance for income transactions based on frequency."""
-    #     logger.info(f"Updating balance for income transaction: {transaction}")
-    #     logger.info(f"Balance dict: {transaction['date_of_transaction']}")
-    #     date_of_transaction = self.parse_date(transaction['date_of_transaction'])
-    #     start_date_trans = self.parse_date(transaction['start_date'])
-    #     end_date_trans = self.parse_date(transaction['end_date']) or datetime.max
-
-    #     if transaction['frequency'] == 'one-time':
-    #         # One-time income transaction
-    #         if date_of_transaction and start_date_trans <= date_of_transaction <= end_date_trans:
-    #             amount_change = transaction['amount']
-    #             current_balance = self.balance_dict[date_of_transaction]['balance']
-    #             new_balance = current_balance + amount_change
-    #             self.balance_dict[date_of_transaction]['balance'] = new_balance
-
-    #     elif transaction['frequency'] in ['weekly', 'bi-weekly']:
-    #         # Weekly or bi-weekly income transaction
-    #         interval_days = 7 if transaction['frequency'] == 'weekly' else 14
-    #         current_date = start_date_trans
-            
-    #         if current_date.weekday() != transaction['day'] - 1:  # transaction['day'] is 1-7 (Mon-Sun)
-    #             days_ahead = (transaction['day'] - 1 - current_date.weekday()) % 7
-    #             current_date += timedelta(days=days_ahead)
-            
-    #         while current_date <= end_date_trans:
-    #             if current_date in self.balance_dict:
-    #                 amount_change = transaction['amount']
-    #                 current_balance = self.balance_dict[current_date]['balance']
-    #                 new_balance = current_balance + amount_change
-    #                 self.balance_dict[current_date]['balance'] = new_balance
-                
-    #             current_date += timedelta(days=interval_days)
-        
-    #     elif transaction['frequency'] == 'semi-monthly':
-    #         # Semi-monthly income transaction
-    #         first_transaction_day = self.parse_date(transaction['date_of_transaction'])
-    #         second_transaction_day = self.parse_date(transaction.get('date_of_second_transaction'))
-
-    #         current_month_start = start_date_trans
-
-    #         while current_month_start <= end_date_trans:
-    #             # Check if we are in the current month
-    #             if current_month_start.month == start_date_trans.month and current_month_start.year == start_date_trans.year:
-    #                 # If start date is after the first transaction day, skip it
-    #                 if start_date_trans > first_transaction_day:
-    #                     # Only update for the second transaction day if it falls within the range
-    #                     if second_transaction_day and second_transaction_day.month == current_month_start.month and second_transaction_day.year == current_month_start.year:
-    #                         amount_change = transaction['amount']
-    #                         current_balance = self.balance_dict[second_transaction_day]['balance']
-    #                         new_balance = current_balance + amount_change
-    #                         self.balance_dict[second_transaction_day]['balance'] = new_balance
-    #                 else:
-    #                     # Update for the first transaction day if it falls within the range
-    #                     if first_transaction_day and first_transaction_day.month == current_month_start.month and first_transaction_day.year == current_month_start.year:
-    #                         amount_change = transaction['amount']
-    #                         current_balance = self.balance_dict[first_transaction_day]['balance']
-    #                         new_balance = current_balance + amount_change
-    #                         self.balance_dict[first_transaction_day]['balance'] = new_balance
-                        
-    #                     # Update for the second transaction day if it falls within the range
-    #                     if second_transaction_day and second_transaction_day.month == current_month_start.month and second_transaction_day.year == current_month_start.year:
-    #                         amount_change = transaction['amount']
-    #                         current_balance = self.balance_dict[second_transaction_day]['balance']
-    #                         new_balance = current_balance + amount_change
-    #                         self.balance_dict[second_transaction_day]['balance'] = new_balance
-                
-    #             # Move to the next month
-    #             next_month_start = (current_month_start.replace(day=1) + timedelta(days=31)).replace(day=1)
-    #             current_month_start = next_month_start
-
-    #     elif transaction['frequency'] == 'monthly':
-    #         # Monthly income transaction
-    #         transaction_date = self.parse_date(transaction['date_of_transaction'])
-    #         current_month_start = start_date_trans
-            
-    #         while current_month_start <= end_date_trans:
-    #             if end_date_trans.day > transaction_date.day:
-    #                 amount_change = transaction['amount']
-    #                 current_balance = self.balance_dict[transaction_date]['balance']
-    #                 new_balance = current_balance + amount_change
-    #                 self.balance_dict[transaction_date]['balance'] = new_balance
-                
-    #             # Move to the next month
-    #             next_month_start = (current_month_start.replace(day=1) + timedelta(days=31)).replace(day=1)
-    #             current_month_start = next_month_start
 
     def convert_balance_keys_to_string(self, balance_dict: Dict[datetime, Dict[str, any]]) -> Dict[str, Dict[str, any]]:
         """Convert the keys of a balance dictionary from datetime to string format 'mm-dd-yyyy'."""
@@ -170,9 +67,23 @@ class MainService:
         day = min(source_date.day, monthrange(year, month)[1])
         return datetime(year, month, day)
     
-    # def date_range(self, start_date, end_date):
-    #     for n in range(int((end_date - start_date).days) + 1):
-    #         yield start_date + timedelta(n)
+    def add_month_if_possible(self, original_date, current_date):
+        
+        next_month = current_date.month + 1
+        next_year = current_date.year
+        
+        if next_month > 12:
+            next_month = 1
+            next_year += 1
+            
+        last_day_of_next_month = calendar.monthrange(next_year, next_month)[1]
+
+        if original_date.day <= last_day_of_next_month:
+            new_date = current_date.replace(year=next_year, month=next_month, day=original_date.day)
+        else:
+            new_date = current_date.replace(year=next_year, month=next_month, day=last_day_of_next_month)
+        
+        return new_date
             
     def date_range(self, start_date_str, end_date_str):
         start_date = self.parse_date(start_date_str.strftime("%m-%d-%Y"))
@@ -215,12 +126,6 @@ class MainService:
                     recurring_transactions[trans_date].append(transaction)
 
             elif transaction.frequency in ['weekly', 'bi-weekly']:
-                # trans_date = self.parse_date(transaction.start_date)
-                # interval_days = 7 if transaction.frequency == 'weekly' else 14
-                # if trans_date.weekday() != transaction.day - 1:
-                #     days_ahead = (transaction.day - 1 - trans_date.weekday()) % 7
-                    
-                #     trans_date += timedelta(days=days_ahead)
                 trans_date = self.parse_date(transaction.start_date)
                 interval_days = 7 if transaction.frequency == 'weekly' else 14
                 target_weekday = transaction.day - 1  # Adjust for 0-indexed weekday
@@ -260,6 +165,7 @@ class MainService:
 
             elif transaction.frequency == 'monthly':
                 trans_date = self.parse_date(transaction.date_of_transaction)
+                orignal_date = self.parse_date(transaction.date_of_transaction)
                 
                 while trans_date <= end_window:
                     if trans_date >= start_window and trans_date <= end_date:
@@ -267,7 +173,8 @@ class MainService:
                         recurring_transactions[trans_date].append(transaction)
                         
                     # Move to the next month for monthly transactions
-                    trans_date = self.add_months(trans_date, 1)
+                    trans_date = self.add_month_if_possible(orignal_date, trans_date)
+                    # trans_date = self.add_months(trans_date, 1)
 
         return dict(sorted(transaction_dates.items())), recurring_transactions
     
@@ -306,7 +213,9 @@ class MainService:
                     unpaid_expenses.append(expense)
                     overdraft += expense.amount - available_balance
                     available_balance = Decimal('0')
-                    break  # Stop processing further expenses
+                    # break  # Stop processing further expenses
+                    if available_balance < 0:
+                        break
 
             # Prepare the result for this day
             day_result = {
@@ -337,14 +246,6 @@ class MainService:
         logger.info(f"Calculating balances for user: {self.user_id}")
         transactions = get_transaction_service().list_user_transactions(self.user_id)
         self.separate_transactions_by_type(transactions)
-        
-        # for transaction in self.income_transactions:
-        #     self.update_balance_for_income(transaction)
-        # self.update_balance_for_income(self.income_transactions, self.balance_dict)
-        
-        # return self.convert_balance_keys_to_string(self.balance_dict)
-        # recurring_income = self.calculate_recurring_dates(self.income_transactions)
-        # recurring_expense = self.calculate_recurring_dates(self.expense_transactions)
         today = datetime.now()
         start_window = today - timedelta(days=30)
         end_window = today + timedelta(days=210)
